c2727c50ac0cd26242d94f23bdb4c132d1f75af0
Jia Wei Chua <jiawei2912@yahoo.com.sg>
2023-02-06 11:55:37 +0800

    Automatic module loader implemented

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
-import sys
+import sys, inspect
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
 from PyUI import MainUI, LandingPageButton
+import AppModules
 from AppModules import ExampleModule, SimpleInterest, CompoundInterest
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -17,8 +18,22 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.backButton.clicked.connect(lambda : self.navigateTo(0))
         self.ui.backButton.setVisible(False)
 
-    # Todo (if bothered enough): Make this dynamically load all modules in the AppModules folder
+    # ToDo: Add error handling
     def _loadAppModules(self):
+        sub_modules = inspect.getmembers(AppModules, inspect.ismodule)
+        sub_modules = [sub_module[1] for sub_module in sub_modules]
+        moduleWidgetClasses = []
+        for sub_module in sub_modules:
+            classes = inspect.getmembers(sub_module, inspect.isclass)
+            classes = [cls[1] for cls in classes]
+            moduleWidgetClasses.extend(list(filter(lambda cls: issubclass(cls, QtWidgets.QWidget), classes)))
+        for i, moduleWidgetClass in enumerate(moduleWidgetClasses):
+            moduleWidget = moduleWidgetClass()
+            self.ui.bodyStackedWidget.addWidget(moduleWidget)
+            LandingPagePushButton(self, moduleWidget.title, i+1)
+
+    # Old (can delete if the method above does not cause any problems)
+    def _loadAppModules_OLD(self):
         moduleWidgets = []
         moduleWidgets.append(ExampleModule.ExampleModule())
         moduleWidgets.append(SimpleInterest.SimpleInterestCalculator())
