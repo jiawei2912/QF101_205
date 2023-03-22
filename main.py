eeb2d1f1c33ae0359cb2027a252a9bb30bd544c3
Jia Wei Chua <jiawei2912@yahoo.com.sg>
2023-03-22 16:38:20 +0800

    auto-resizing buttons fixed

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.landingPageVLayout.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter)
         self._setUpButtonCallbacks()
         self._loadAppModules()
+
+        # jank ass resizing
+        largest_width = 0
+        for lppButtonIdx in range(self.ui.landingPageVLayout.count()):
+            lppButton = self.ui.landingPageVLayout.itemAt(lppButtonIdx).widget()
+            largest_width = max(largest_width, lppButton.width())
+        for lppButtonIdx in range(self.ui.landingPageVLayout.count()):
+            lppButton = self.ui.landingPageVLayout.itemAt(lppButtonIdx).widget()
+            lppButton.resize(largest_width, lppButton.height())
+            lppButton.ui.appButton1.resize(largest_width, lppButton.height())
+        # Slightly dangerous, as a module with a too long name might result in a window that
+        # is too large and which cannot be shurnk
+        self.setMinimumWidth(int(largest_width*1.1))
+            
         
     # def resizeEvent(self, event:QtGui.QResizeEvent) -> None:
     #     old_size = event.oldSize()
@@ -52,17 +66,22 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.headerLabel.setText(newHeaderLabel)
             self.ui.header_separator.setVisible(True)
 
-
+# Consider adding either a maximum length ot the appName or enforcing wordwrap past a certain length
 class LandingPagePushButton(QtWidgets.QWidget):
     def __init__(self, mainWindow:MainWindow, appName:str, appIndex:int) -> None:
         super().__init__()
-        mainWindow.ui.landingPageVLayout.addWidget(self)
+        mainWindow.ui.landingPageVLayout.addWidget(self, stretch=1)
         self.ui = LandingPageButton.Ui_LandingPageButton()
         self.ui.setupUi(self)
         self.ui.appButton1.setText(appName)
         self.ui.appButton1.clicked.connect(lambda : mainWindow.navigateTo(appIndex, appName))
+        self.ui.appButton1.adjustSize()
+        self.adjustSize()
+    # Needed for the widgets to appear; 
+    # supposedly because the main widget does not have an activated layout
     def sizeHint(self):
-        return QtCore.QSize(241, 33)
+        #return QtCore.QSize(int(self.parent().width()), self.ui.appButton1.height())
+        return self.ui.appButton1.size()
 
 
 def main():
